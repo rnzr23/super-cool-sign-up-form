d5450b2695b53820c9673715a425ff6710615178
Sarah <sranzieri@fi.uba.ar>
2022-09-24 00:01:14 +0200

    first version of the form

---
+++ validation.js
@@ -0,0 +1,11 @@
+var password = document.getElementById('password');
+confirm_password = document.getElementById('cpassword');
+    function validatePassword() {
+        if ((confirm_password.value!='')&&(password.value != confirm_password.value)) {
+            document.getElementById('errorMsg').innerHTML='* Passwords do not match';
+        } else {
+            document.getElementById('errorMsg').innerHTML='';
+        }
+    }
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
